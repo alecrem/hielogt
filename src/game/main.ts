a99b07e47783abdd9e993b1e224f8502f6be8416
Alejandro Cremades <ale@alecrem.com>
2025-05-05 17:24:51 +0900

    Format documents; adjust aspect ratio for mobile portrait

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,31 +1,29 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from "./scenes/Boot";
+import { GameOver } from "./scenes/GameOver";
+import { Game as MainGame } from "./scenes/Game";
+import { MainMenu } from "./scenes/MainMenu";
+import { AUTO, Game } from "phaser";
+import { Preloader } from "./scenes/Preloader";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: AUTO,
+  width: 390,
+  height: 640,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  pixelArt: true,
+  autoRound: true,
 };
 
 const StartGame = (parent: string) => {
-
-    return new Game({ ...config, parent });
-
-}
+  return new Game({ ...config, parent });
+};
 
 export default StartGame;
